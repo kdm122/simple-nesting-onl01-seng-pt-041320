b24ae057439eaa4033a02e17de15631734eb6916
Kyle McLean <kyledmclean@gmail.com>
2020-04-26 04:58:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -94,7 +94,7 @@ def adding_matz
 
 #hash[key] = { dollar: 0 }
 
- programmer_hash[:yukihiro_matsumoto] = { :known_for => "Ruby", :languages => ["LISP","C"] }
+ programmer_hash[] = { :yukihiro_matsumoto { :known_for => "Ruby", :languages => ["LISP","C"] }}
 end
 
 def changing_alan
